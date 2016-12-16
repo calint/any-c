9fc430e159eb2816a7fe857fb5b534d407b71caf
calint <calin.tenitchi@gmail.com>
2016-12-16 22:05:50 +0700

    .

--- src/ci/expr_ident.h
+++ src/ci/expr_ident.h
@@ -16,35 +16,30 @@ inline static void _ci_expr_ident_free_(struct ci_expr*oo){
 }
 
 inline static void _ci_expr_ident_compile_(const ci_expr*oo,ci_toc*tc){
-
-	ci_expr_ident*o=(ci_expr_ident*)oo;
+	const ci_expr_ident*o=(ci_expr_ident*)oo;
 	const char idtype=ci_toc_find_ident_type(tc,o->name.data);
 	if(idtype=='c'){// class member
 		printf("o->%s",o->name.data);
-		o->cnst=0;
 		return;
 	}
 	if(idtype){// local identifier
 		printf("%s",o->name.data);
-		o->cnst=0;
-		return;
-	}
-
-	//
-	if(strcmp("true",o->name.data)){
-		printf("true");
-		return;
-	}
-
-	if(strcmp("false",o->name.data)){
-		printf("false");
 		return;
 	}
 
 	// constant
-	o->cnst=1;
+	if(!strcmp("true",o->name.data)){
+		printf("true");
+		return;
+	}
+
+	if(!strcmp("false",o->name.data)){
+		printf("false");
+		return;
+	}
+
 	char* endptr;
-	o->cnst=strtol(o->name.data,&endptr,10);
+	strtol(o->name.data,&endptr,10);
 	if(*endptr!=0){
 		// not end of string
 		printf("\n\n<file> <line:col> '%s' is not found and could not "
@@ -52,7 +47,7 @@ inline static void _ci_expr_ident_compile_(const ci_expr*oo,ci_toc*tc){
 		exit(1);
 
 	}
-	str_setz(&o->super.type,"int");
+//	str_setz(&o->super.type,"int");
 	printf("%s",o->name.data);
 }
 
