e29ddb2b7f436cbed766ae0046144abb9d3a95de
calint <calin.tenitchi@gmail.com>
2016-12-17 18:48:49 +0700

    .

---
+++ c.c
@@ -0,0 +1,23 @@
+//--- - - -------------------  - -- - - - - - - -- - - -  generated c source
+//--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -
+#include<stdlib.h>
+#include<stdio.h>
+typedef char bool;
+typedef const char* ccharp;
+#define true 1
+#define false 1
+#define char_def 0
+#define int_def 0
+#define float_def 0.0f
+#define bool_def false
+//--- - - -------------------  - -- - - - - - - -- - - - -- - - - --  global
+typedef struct global{}global;
+#define global_def (global){}
+//--- - - -------------------  - -- - - - - - - -- - - - -- - - - -- - funcs
+inline static void global_main(global*o,int c,char**a ){
+	int b=1;
+	printf("%d. hello world",b);
+}
+//--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -
+int main(int c,char** a){global_main(0,c,a);}
+//--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -
