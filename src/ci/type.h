5245ca2da7fd78d11815ea48951c897cbef8868c
calint <calin.tenitchi@gmail.com>
2016-12-17 19:19:48 +0700

    .

--- src/ci/type.h
+++ src/ci/type.h
@@ -6,25 +6,28 @@ typedef struct field{
 	str type;
 	str name;
 	xexpr*initval;
+	token token;
 }field;
 
-#define field_def (field){str_def,str_def,NULL}
+#define field_def (field){str_def,str_def,NULL,token_def}
 
 typedef struct funcarg{
 	str type;
 	str name;
+	token token;
 }funcarg;
 
-#define funcarg_def (funcarg){str_def,str_def}
+#define funcarg_def (funcarg){str_def,str_def,token_def}
 
 typedef struct func{
 	str type;
 	str name;
 	dynp args;
 	code code;
+	token token;
 }func;
 
-#define func_def (func){str_def,str_def,dynp_def,code_def}
+#define func_def (func){str_def,str_def,dynp_def,code_def,token_def}
 
 typedef struct type{
 	str name;
