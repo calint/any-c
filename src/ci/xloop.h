faf25b3145f2331291de1f3452b89c46e9bea0aa
calint <calin.tenitchi@gmail.com>
2016-12-18 13:38:17 +0700

    .

--- src/ci/xloop.h
+++ src/ci/xloop.h
@@ -11,7 +11,7 @@ inline static void _xloop_compile_(const xexpr*oo,toc*tc){
 	o->code.super.compile((xexpr*)&o->code,tc);
 }
 
-#define xloop_def (xloop){{_xloop_compile_,NULL,str_def,token_def,0},code_def}
+#define xloop_def (xloop){{_xloop_compile_,NULL,str_def,token_def,1},code_def}
 
 inline static xloop*xloop_read_next(toc*tc){
 	toc_push_scope(tc,'l',"");
