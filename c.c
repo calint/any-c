62fda9d2e52f30bffc54f755091e8fd09831f883
calint <calin.tenitchi@gmail.com>
2016-12-21 09:11:46 +0700

    .

--- c.c
+++ c.c
@@ -11,78 +11,32 @@ typedef char bool;
 #define char_def 0
 #define int_def 0
 #define float_def 0.0f
-//--- - - -------------------  - -- - - - - - - -- - - - -- - - - -- - -  id
-typedef struct id{
-    int i;
-}id;
-#define id_def (id){0}
-//--- - - -------------------  - -- - - - - - - -- - - - -- - - - -- - funcs
-inline static void id__init(id*o){
-	printf("init id\n");
-	o->i=1;
-}
-
-inline static void id__free(id*o){
-	printf("free id\n");
-}
-
-inline static void id_init(id*o){
-    id__init(o);
-}
-inline static void id_free(id*o){
-    id__free(o);
-}
 //--- - - -------------------  - -- - - - - - - -- - - - -- - - - --  entity
 typedef struct entity{
-    id id;
+    int id;
 }entity;
-#define entity_def (entity){id_def}
+#define entity_def (entity){1}
 //--- - - -------------------  - -- - - - - - - -- - - - -- - - - -- - funcs
-inline static void entity__init(entity*o){
-	printf("init entity\n");
+inline static void entity_print(entity*o,entity e){
+	printf("entity: %d\n",e.id);
 }
 
-inline static void entity_print(entity*o){
-	printf("entity: %i\n",o->id.i);
-}
-
-inline static void entity__free(entity*o){
-	printf("free entity\n");
-}
-
-inline static void entity_init(entity*o){
-    id_init(&o->id);
-    entity__init(o);
-}
-inline static void entity_free(entity*o){
-    entity__free(o);
-    id_free(&o->id);
-}
 //--- - - -------------------  - -- - - - - - - -- - - - -- - - - --  global
-typedef struct global{
-    entity e;
-}global;
-#define global_def (global){entity_def}
+typedef struct global{}global;
+#define global_def (global){}
 //--- - - -------------------  - -- - - - - - - -- - - - -- - - - -- - funcs
-inline static void global__init(global*o){
-	printf("init global\n");
-}
-
 inline static void global_main(global*o){
-	entity_print((entity*)&o->e);
-}
-
-inline static void global__free(global*o){
-	printf("free global\n");
+	entity e1=entity_def;
+	e1.id=1;
+	entity e2=entity_def;
+	e2.id=2;
+	entity_print((entity*)&e1,e2);
+	entity_print((entity*)&e2,e1);
 }
 
 inline static void global_init(global*o){
-    entity_init(&o->e);
-    global__init(o);
 }
 inline static void global_free(global*o){
-    global__free(o);
-    entity_free(&o->e);
 }
 //--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -
 int main(int c,char**a){
